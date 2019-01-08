80821042d8a36897171ed5dd84f2a0a84688d823
Jovan Bogoievski <jovan.bogoievski@sjs.co.nz>
2019-01-09 12:55:28 +1300

    updated dropdown responsiveness

--- src/components/item/styles.js
+++ src/components/item/styles.js
@@ -1,9 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  container: {
-    paddingVertical: 8,
-    borderRadius: 0,
-    justifyContent: 'center',
-  },
+	container: {
+		paddingVertical: 8,
+		borderRadius: 0,
+		justifyContent: 'center',
+		backgroundColor: '#fff'
+	},
 });
